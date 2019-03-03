5ad12951d0421396083af2631cce22079e75c429
alekseypivovar <you@example.com>
2019-03-03 19:45:47 +0300

    Compressor/decompressor v0.1 'Kostil'

---
+++ src/decompressorcpp.cpp
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "header.h"
+#include <sys/stat.h>
+
+int decompress(char* filename)
+{
+	FILE *fp = fopen(filename, "rb");				// take filename from cmd
+	if (fp == NULL)									// check file opening
+	{
+		puts("ERROR! Cannot read file!");
+		return 1;
+	}
+	
+	struct SYM buf[N*2];							// structure array for letters
+	initializeArray(buf);							// fill each structure in array
+	int tail;
+
+	char arr[N];									// array for reading from file
+	
+	//fscanf(fp, "%s", arr);						// read first word
+	fread(arr, sizeof(char), 6, fp);
+	arr[6] = '\0';
+	if (strcmpi(arr, "pivrar") != NULL)				// check signature
+	{
+		puts("ERROR! That is not a .pivrar file!");
+		return 1;
+	}
+	long symCounter;
+	fread(&symCounter, sizeof(long), 1, fp);
+	long long i = 0;
+
+	while (i<256)									// read 'ch' and 'code'
+	{
+		fread(&buf[i].freq, sizeof(float), 1, fp);
+	//	if (buf[i].freq == 0.0)
+	//		break;
+		fread(&buf[i].ch, sizeof(unsigned char), 1, fp);
+		i++;
+	}
+	i = 0;
+	fread(&tail, sizeof(int), 1, fp);
+	//fscanf(fp, "%d", &tail);						// read tail lenght
+
+	char filename_decomp[32];
+	filename_decomp[0] = 'd';						// filename
+	int filenameLenght;
+	//fscanf(fp, "%s", &(filename_decomp[1]));
+	fread(&filenameLenght, sizeof(int), 1, fp);
+	fread(&filename_decomp[1], sizeof(char), filenameLenght, fp);
+	filename_decomp[1 + filenameLenght] = '\0';
+
+	FILE *fp_101 = fopen("decomp101.101", "wb");
+	if (fp_101 == NULL)								// check file opening
+	{
+		puts("ERROR! Cannot create decompressed .101 file!");
+		return 1;
+	}
+
+	unsigned char letter;							// read main part of compessed file 
+	unsigned char string[8];						// and write it to .101 file
+	
+	long counter=0;
+	while (counter<symCounter)	
+	{
+		fread(&letter, 1, 1, fp);
+		unpack(string, letter);
+		fwrite(string, sizeof(unsigned char), sizeof(string), fp_101);
+		counter++;
+	}
+	counter = 0;
+
+	fclose(fp);
+	fclose(fp_101);
+	FILE *fp_101_2 = fopen("decomp101.101", "rb");	// open .101 file again
+	
+	struct SYM *psym[N * 2];
+	for (int i = 0; i < N * 2; i++)
+	{
+		psym[i] = &buf[i];
+	}
+	struct SYM *root;
+	root = buildTree(psym, N);
+
+	makeCodes(root);
+
+	FILE *fp_decomp = fopen(filename_decomp, "w");
+	if (fp_decomp == NULL)							// check decompressed file opening
+	{
+		puts("ERROR! Cannot create decompressed file!");
+		return 1;
+	}
+
+	char code_101[N * 2];
+	char letter_decomp;
+	i = 0;
+	while (counter < (symCounter))				// read .101 without last symbol and write to decompressed file
+	{
+		fread(&code_101[i], sizeof(char), 1, fp_101_2);
+		code_101[i + 1] = '\0';
+		for (int j = 0; j < N; j++)					// find letter
+		{
+			if (strcmp(code_101, buf[j].code) == 0)
+			{
+				fwrite(&buf[j].ch, sizeof(unsigned char), 1, fp_decomp);
+				i = -1;
+				counter++;
+				break;
+			}
+			else if (buf[j].freq == 0.0)
+			{
+				break;
+				//i++;
+			}
+		}
+		i++;
+		
+	}
+	/*
+	while (counter < (symCounter))					// read .101 without last symbol and write to decompressed file
+	{
+		fread(&code_101[i], sizeof(char), 1, fp_101_2);
+		code_101[i + 1] = '\0';
+		for (int j = 0; j < N; j++)					// find letter
+		{
+			if (strcmp(code_101, buf[j].code) == 0)
+			{
+				fwrite(&buf[j].ch, sizeof(unsigned char), 1, fp_decomp);
+				i = -1;
+				counter++;
+				break;
+			}
+			else if (buf[j].freq == 0.0)
+			{
+				break;
+				//i++;
+			}
+		}
+		i++;
+
+	}
+	*/
+	fclose(fp_101_2);
+	fclose(fp_decomp);
+	return 0;
+}
+
+
+unsigned char* unpack(unsigned char *string, unsigned char letter)
+{
+	union CODE code;
+	code.ch = letter;
+	string[0] = code.byte.b1 + '0';
+	string[1] = code.byte.b2 + '0';
+	string[2] = code.byte.b3 + '0';
+	string[3] = code.byte.b4 + '0';
+	string[4] = code.byte.b5 + '0';
+	string[5] = code.byte.b6 + '0';
+	string[6] = code.byte.b7 + '0';
+	string[7] = code.byte.b8 + '0';
+	return string;
+}
---
+++ src/main.cpp
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "header.h"
+
+// to compress write in CMD "filename.txt compress"
+// to decompress write in CMD "filename.txt decompress"
+
+/*
+struct SYM			
+{
+	unsigned char ch;
+	float freq;
+	char code[256];
+	struct SYM *left;
+	struct SYM *right;
+};
+*/
+
+int main(int argc, char* argv[])
+{
+	// analyzator:
+
+	if (argc < 2)									// check arguments
+	{
+		puts("Error! Not enough arguments! Please enter a filename in cmd for compress it!");
+		puts("To compress file enter 'filename.txt compress' in CMD");
+		puts("To decompress file enter 'filename.txt decompress' in CMD");
+		puts("In theory file extension may be different (not only .txt) =)");
+		puts("If you want to buy a full version please call 8-800-555-35-35");
+		return 1;
+	}
+	
+	if (strcmpi(argv[2], "compress") == NULL)		// COMPRESS
+	{
+
+		FILE *fp = fopen(argv[1], "rb");			// take filename from cmd
+		if (fp == NULL)								// check file opening
+		{
+			puts("ERROR! Cannot read file!");
+			return 1;
+		}
+
+		struct SYM buf[N * 2] = { 0 };				// structure array for letters
+		initializeArray(buf);						// fill each structure in array
+
+		char letter;								// one letter from file
+		long long count = 0;						// how much letters in the file
+
+		while (!feof(fp))
+		{
+			count++;
+			letter = fgetc(fp);
+			//fread(&letter, sizeof(char), 1, fp);
+			makeSymArray(buf, letter);
+		}
+		fclose(fp);
+
+		qsort(buf, N, sizeof(struct SYM), comp);
+
+		calculateFreq(buf, count);		// redefine count of letters to part of 1 (calculate frequency)
+
+		printArray(buf);
+
+
+		// code generation:
+
+		struct SYM *psym[N * 2];
+		for (int i = 0; i < N * 2; i++)
+		{
+			psym[i] = &buf[i];
+		}
+		struct SYM *root;
+		root = buildTree(psym, N);
+
+		printf("Root frequency (in all tree) = %f\n", root->freq);
+		makeCodes(root);
+
+		// make .101 file:
+
+		FILE *fp_in = fopen(argv[1], "rb");
+		FILE *fp_101 = fopen("101.101", "wb");					// coded .101 file
+		if (fp_101 == NULL)										// check file opening
+		{
+			puts("ERROR! Cannot create .101 file!");
+			return 1;
+		}
+
+		unsigned long long count_101 = 0;
+		long symCounter=0;
+		while ((letter = fgetc(fp_in)) != -1)
+		{
+			for (int i = 0; i < N; i++)
+				if (buf[i].ch == (unsigned char)letter)
+				{
+				//	fputs(buf[i].code, fp_101);				// write symbol code into .101 file
+					fwrite(&buf[i].code, sizeof(char), strlen(buf[i].code), fp_101);
+					count_101+=strlen(buf[i].code);
+					symCounter++;
+					break;
+				}
+		}
+		int tail = 8-(count_101 % 8);							// tail lenght
+		char tailLetter = '1';
+		if (tail > 0)										// write tail to the .101 file
+		{
+			for (int k = 0; k < tail;k++)
+				fwrite(&tailLetter, sizeof(char), 1, fp_101);
+				//fputs("1", fp_101);
+		}
+
+		fclose(fp_in);
+		fclose(fp_101);
+
+		// pack file:
+
+		int error = packFile(buf, tail, argv[1], symCounter);
+		if (error == 1)
+			return 1;
+	}
+	else if (strcmpi(argv[2], "decompress") == NULL)		// DE-COMPRESS
+	{
+	int decError=decompress(argv[1]);
+	if (decError == 1)
+		return 1;
+
+
+	}
+	else													// ERROR
+	{
+		puts("ERROR! UNKNOWN COMMAND!");
+		return 1;
+	}
+
+	puts("Done successfully!");
+	return 0;
+}
