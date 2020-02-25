97718bab0af629c71c5be7a5fc4ee61932bf0dcf
dkrasnykh <krasnykh.daria@gmail.com>
2020-02-25 11:18:07 +0300

    B30

---
+++ src/main2.cpp
@@ -0,0 +1,88 @@
+﻿#include <stdio.h>
+#include "task2.h"
+
+int main(int argc, char* argv[])
+{
+
+	FILE* fp;
+	fp = fopen(argv[1], "rb");
+	struct SYM syms[LEN];
+	struct SYM* psyms[MAX_LEN]; 
+	unsigned char buf1[3];
+	unsigned char chns;
+	
+	fread(buf1, 2, 1, fp);
+	fread(&chns, sizeof(unsigned char), 1, fp);
+	buf1[2] = '\0';
+	int ns = (int)chns;
+
+	for (int i = 0; i < ns; i++)
+	{
+		fread(&(syms[i].ch), sizeof(unsigned char), 1, fp);
+		fread(&(syms[i].freq), sizeof(double), 1, fp);
+		syms[i].code[0] = 0;
+		syms[i].left = NULL;
+		syms[i].right = NULL;
+		psyms[i] = syms + i;
+	}
+	
+	psyms[ns + 1] = NULL;
+	unsigned char chtail;
+	fread(&chtail, sizeof(unsigned char), 1, fp);
+	int tail = (int)chtail;
+	int size_original_file = 0;
+	fread(&size_original_file, sizeof(int), 1, fp);
+	unsigned char ext[4];
+	fread(ext, 3, 1, fp);
+	ext[3] = '\0';
+	struct SYM* root = (struct SYM*)malloc(sizeof(struct SYM));
+	root = buildTree(psyms, ns); 
+	makeCodes(root);
+	FILE* fp_101;
+	fp_101 = fopen("fp_101.txt", "wb");
+	unsigned char ch;
+	unsigned char buf[9];
+	int count = 0;
+	int c = 0;
+
+	while (fread(&ch, sizeof(unsigned char), 1, fp))
+	{
+		unpack(ch, buf);
+		fwrite(buf, 8, 1, fp_101);
+		count += 8;
+	}
+	count = count - 8 + tail; 
+	fclose(fp);
+	fclose(fp_101);
+	fp_101 = fopen("fp_101.txt", "rb");
+	FILE* fres;
+	fres = fopen("result.txt", "wb");
+	int i = 0;
+	char code[LEN];
+	int lenc = 0;
+	unsigned char temp;
+
+	while (fread(&ch, sizeof(unsigned char), 1, fp) && i<count)
+	{
+		code[lenc] = ch;
+		lenc++;
+		code[lenc] = '\0';
+		temp= searchTree(root, code);
+		if (temp != 0)
+		{
+			fwrite(&temp, 1, 1, fres);
+			lenc = 0;
+			code[0] = '\0';
+		}
+		i++;
+	}
+
+	int size_result_file;
+	size_result_file = ftell(fres);
+
+	if (size_original_file - size_result_file != 0)
+		printf("The size of the source file is %d, and the size of the file with the result is %d!\n", size_original_file, size_result_file);
+
+	fclose(fres);
+	return 0;
+}
---
+++ src/task2.cpp
@@ -0,0 +1,44 @@
+﻿#include <stdio.h>
+#include <string.h>
+#include "task2.h"
+
+unsigned char searchTree(struct SYM* node, char * code)
+{
+
+	if (node == NULL)
+		return 0;
+	if (strcmpi(node->code, code) == 0)
+		return node->ch;
+	char temp[LEN]; 
+	char tempR[LEN];
+	char tempL[LEN];
+	int n = strlen(node->code); 
+	if (n+1 < strlen(code))
+	{
+		strncpy(temp, code, n+1); 
+		temp[n + 1] = '\0';
+	}
+	else strcpy(temp, code);
+
+	strcpy(tempR, node->code);
+	strcat(tempR, "1");
+	strcpy(tempL, node->code);
+	strcat(tempL, "0");
+
+	if (strcmpi(temp, tempL) < 0 || strcmpi(temp, tempL) == 0)
+		searchTree(node->left, code);
+	else if(strcmpi(temp, tempR) > 0 || strcmpi(temp, tempR) == 0)
+		searchTree(node->right, code);
+
+}
+
+void printTree(struct SYM * node)
+{
+	if (node != NULL)
+	{
+		printTree(node->left);
+		if (node->ch != 0)
+			printf("%c  %s\n", node->ch, node->code);
+		printTree(node->right);
+	}
+}
