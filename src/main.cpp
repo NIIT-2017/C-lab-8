665919502e1f46762b18aa5091c6d0ab269611cf
Mikhail Treushnikov <www.mor.ry@mail.ru>
2020-03-06 11:30:03 +0300

    Kompessor is done

---
+++ src/main.cpp
@@ -0,0 +1,200 @@
+﻿/* Данный код реализует алгоритм Хаффмана по сжатию файлов*/
+
+
+#include "task.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(int argc, char** argv)
+{
+	if (strcmp(argv[2], "com") == 0) // сжатие
+	{
+		FILE* fp;
+		FILE* out;
+		FILE* filezip;
+		fp = fopen(argv[1], "rb");
+		out = fopen("fp_101.txt", "wt");// временный файл из 0 и 1
+		{
+			if (fp == NULL)
+			{
+				puts("File open erorr!\n");
+				return 1;
+			}
+		}
+		struct SYM syms[SIZE] = { 0 };
+		struct SYM* psyms[SIZE] = { 0 };
+		int ch;
+		int count = 0, count1 = 0;
+		while ((ch = fgetc(fp)) != EOF)//по символьно 
+		{
+			count++;
+			for (int i = 0; i < SIZE; i++)
+			{
+				if (ch == syms[i].ch)
+				{
+					syms[i].freq++;
+					break;
+				}
+				else if (syms[i].ch == 0)
+				{
+					syms[i].ch = ch;
+					syms[i].freq++;
+					count1++;
+					break;
+				}
+			}
+		}
+		for (int i = 0; i < SIZE; i++)
+			syms[i].freq = syms[i].freq / count;
+		rewind(fp);
+		qsort(syms, count1, sizeof(syms[0]), cmp);// сортировка
+		for (int i = 0; i < SIZE; i++)
+			psyms[i] = &syms[i];// заполняем массив указателей адресами структуры SYM
+		struct SYM* root = buildTree(psyms, count1);
+		makeCodes(root);
+		ch = 0;
+		while ((ch = fgetc(fp)) != EOF)
+		{
+			for (int i = 0; i < count1; i++)
+				if (syms[i].ch == (unsigned char)ch)
+				{
+					fputs(syms[i].code, out); // выводим строку с кодом
+					break; // прерываем поиск
+				}
+		}
+		ch = 0;
+		fclose(out);
+		out = fopen("fp_101.txt", "rb"); 
+		long int count2 = filesize(out);// получившйся count размер файла из 0 и 1
+		count2 = count2%8; // остаток от деления на 8
+		rewind(out);
+		char bufname[256] = { 0 }; // массив для записи имя файла
+		for (int i = 0; argv[1][i] != '.'; i++)
+			bufname[i] = argv[1][i];
+		char bufname2[5] = { ".tmv" }; // формат сжатого файла
+		filezip = fopen(strcat(bufname, bufname2), "wb");
+		fwrite("tmv", sizeof(char), 3, filezip); // записываю сигнатуру
+		fwrite(&count1, sizeof(unsigned char), 1, filezip);// количество символов в таблице встречаемости
+		for (int i = 0; i < count1; i++)
+		{
+			fwrite(&syms[i].ch, sizeof(unsigned char), 1, filezip);// записываю таблицу встречаемости - символ
+			fwrite(&syms[i].freq, sizeof(float), 1, filezip);// записываю таблицу встречаемости - частота
+		}
+		fwrite(&count2, sizeof(unsigned char), 1, filezip);//длина хвоста - размер файла из 0 и 1 деленный на 8
+		fwrite(&count, sizeof(int), 1, filezip);// размер иссходного сжимаего файла
+		for (int i = 0; argv[1][i] != '\0'; i++) // записываем формат исходного файла
+			if (argv[1][i] == '.')
+			{
+				for (int j = i; argv[1][j] != '\0'; j++)
+					fwrite(&argv[1][j], sizeof(char), 1, filezip);
+				break;
+			}
+		ch = 0;
+		unsigned char buf[8] = { 0 };
+		int count4 = 0;
+		while ((ch = fgetc(out)) != EOF)
+		{
+			if (count4 == 8)
+			{
+				unsigned char a = pack(buf); // считываем по элементно и запаковывваем файл из 0 и 1
+				fwrite(&a, sizeof(unsigned char), 1, filezip);
+				for (int i = 0; i < 8; i++)
+				{
+					if (i == 0)
+						buf[i] = ch;
+					else
+						buf[i] = '\0';
+				}
+				count4 = 1;
+			}
+			else 
+			{
+				buf[count4] = ch;
+				count4++;
+			}
+		}
+		if (count2 != 0) // если остаток от деления на 8 не равен 0, то дополняем хвостом из 0
+		{
+			for (int i = count2; i < 8; i++)
+				buf[i] = '0';
+			unsigned char a = pack(buf);
+			fwrite(&a, sizeof(unsigned char), 1, filezip);
+		}
+		fcloseall();
+		return 0;
+	}
+	else // декомпрессия
+	{
+		FILE* fp;
+		FILE* out;
+		FILE* decomp;
+		fp = fopen(argv[1], "rb");
+		char name[256] = { 0 }; // записываем имя файла
+		for (int i = 0; argv[1][i] != '.'; i++)
+			name[i] = argv[1][i];
+		strcat(name, "1"); //добавляем еденицу вконце имени чтобы ибежать совпадения имен
+		char buf[4] = { 0 };
+		fread(buf, sizeof(char), 3, fp);// считываю формат
+		if (strcmp(buf, "tmv") != 0)
+		{
+			puts("Unknown format!\nExit\n");
+			exit(0);
+		}
+		int count = 0;
+		fread(&count, sizeof(unsigned char), 1, fp); // считываю количесвто уникальных символов
+		struct SYM syms[SIZE] = { 0 };// образую массив структур
+		struct SYM* psyms[SIZE] = { 0 }; // образую массив указателей на структуры
+		for (int i = 0; i < count; i++)
+		{
+			fread(&syms[i].ch, sizeof(unsigned char), 1, fp);// считываю таблицу встречаемости - символ
+			fread(&syms[i].freq, sizeof(float), 1, fp);// считываю таблицу встречаемости - частота
+			psyms[i] = &syms[i];
+		}
+		int count2 = 0;
+		fread(&count2, sizeof(unsigned char), 1, fp); // хвост, остаток от деления на 8
+		struct SYM* root = buildTree(psyms, count);
+		makeCodes(root);
+		int definesize = 0;
+		fread(&definesize, sizeof(int), 1, fp); // считываем исходный размер файла;
+		char buf2[5] = { 0 };
+		fread(buf2, sizeof(char), 4, fp); // формат исходного файла
+		unsigned char buf3[9] = { 0 };
+		unsigned char ch = 0; // считываем по 2 байта чтобы определить последний и байт и обрезать его на остаток хвоста
+		unsigned char ch1 = 0;
+		out = fopen("upfile_101.txt", "wt"); // временный файл из 0 и 1
+		while (fread(&ch, sizeof(unsigned char), 1, fp))
+		{
+			if (count2 != 0)
+			{
+				if (!fread(&ch1, sizeof(unsigned char), 1, fp)) // если байт последний то обрезаем его
+				{
+					unpack(buf3, ch);
+					for(int i = 0;i<count2;i++)
+						fprintf(out, "%c", buf3[i]);
+					break;
+				}
+				fseek(fp, -sizeof(unsigned char), SEEK_CUR); // сдвигаем указатель в файле fp на 1 байт назад
+			}
+			unpack(buf3, ch); // распаковка файла
+			fprintf(out, "%s", buf3); // содаем файл из 0 и 1
+			for (int i = 0; i < 9; i++) // очищаем буфер
+				buf3[i] = '\0';
+		}
+		fclose(out);
+		out = fopen("upfile_101.txt", "rb");
+		decomp = fopen(strcat(name, buf2), "wb");
+		searchTree(root, out, decomp);// ищем в дереве нужное нам сочетание и записываем в файл соответсвующий символ
+		fclose(decomp);
+		decomp = fopen(name, "rb");
+		long int resultsize = filesize(decomp);
+		if (definesize!= resultsize)
+		{
+			puts("Error\n");
+			exit(0);
+		}
+		fcloseall();
+		return 0;
+	}
+}
---
+++ src/task.cpp
@@ -0,0 +1,144 @@
+﻿#include "task.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 256
+
+
+struct SYM* buildTree(struct SYM* psym[], int N) // построения дерева
+{
+	// создаём временный узел
+	struct SYM* temp = (struct SYM*)malloc(sizeof(struct SYM));
+	// в поле частоты записывается сумма частот
+	// последнего и предпоследнего элементов массива psym
+	temp->freq = psym[N - 2]->freq + psym[N - 1]->freq;
+	// связываем созданный узел с двумя последними узлами
+	temp->left = psym[N - 1];
+	temp->right = psym[N - 2];
+	temp->code[0] = 0;
+	if (N == 2) // мы сформировали корневой элемент с частотой 1.0
+		return temp;
+	for (int i = N-2; i > 0; i--)
+	{
+		if (temp->freq >= psym[0]->freq)
+		{
+			for (int j = N - 2; j >= 0; j--)
+				psym[j + 1] = psym[j];
+			psym[0] = temp;
+			break;
+		}
+		else if (temp->freq >= psym[i]->freq && temp->freq <= psym[i - 1]->freq)
+		{
+			for (int j = N - 2; j >= i; j--)
+				psym[j + 1] = psym[j];
+			psym[i]= temp;
+			break;
+		}
+	}
+	return buildTree(psym, N - 1);
+}
+
+
+unsigned char pack(unsigned char buf[]) // функция упаковки
+{
+	union CODE code;
+	code.byte.b1 = buf[0] - '0';
+	code.byte.b2 = buf[1] - '0';
+	code.byte.b3 = buf[2] - '0';
+	code.byte.b4 = buf[3] - '0';
+	code.byte.b5 = buf[4] - '0';
+	code.byte.b6 = buf[5] - '0';
+	code.byte.b7 = buf[6] - '0';
+	code.byte.b8 = buf[7] - '0';
+	return code.ch;
+}
+
+void unpack(unsigned char* buf,unsigned char ch) // функция распаковки
+{
+	union CODE code;
+	code.ch = ch;
+	buf[0] = code.byte.b1+'0';
+	buf[1] = code.byte.b2+'0';
+	buf[2] = code.byte.b3+'0';
+	buf[3] = code.byte.b4+'0';
+	buf[4] = code.byte.b5+'0';
+	buf[5] = code.byte.b6+'0';
+	buf[6] = code.byte.b7+'0';
+	buf[7] = code.byte.b8+'0';
+}
+
+void searchTree(struct SYM* root, FILE* in,FILE*out) // поиск нужного слова в дереве
+{
+	static SYM* rootD = root;
+	if (root->right == NULL && root->left == NULL)
+	{
+		fprintf(out, "%hhc", root->ch);
+		return;
+	}
+	int ch = 0;
+	while ((ch = fgetc(in)) != EOF)
+	{
+		if (ch == '0')
+		{
+			searchTree(root->right, in, out);
+			if (root != rootD)
+				return;
+			else
+				continue;
+		}
+		if (ch == '1')
+		{
+			searchTree(root->left, in, out);
+			if (root != rootD)
+				return;
+			else
+				continue;
+		}
+	}
+}
+
+void makeCodes(struct SYM* root) // создает код из 0 и 1
+{
+	if (root->left)
+	{
+		strcpy(root->left->code, root->code);
+		strcat(root->left->code, "1");
+		makeCodes(root->left);
+	}
+	if (root->right)
+	{
+		strcpy(root->right->code, root->code);
+		strcat(root->right->code, "0");
+		makeCodes(root->right);
+	}
+}
+
+
+int cmp(const void* a, const void* b) // функция компаратора
+{
+	return int(((struct SYM*)b)->freq * 100) - int(((struct SYM*)a)->freq * 100);
+}
+
+void print(struct SYM* syms)
+{
+	for (int i = 0; i < SIZE; i++)
+	{
+		if (syms[i].ch == '\n')
+			printf("\\n - %f\n", syms[i].freq);
+		else if (syms[i].ch == '\r')
+			printf("\\r - %f\n", syms[i].freq);
+		else if (syms[i].ch != 0 && syms[i].ch != '\n')
+			printf("%c - %f\n", syms[i].ch, syms[i].freq);
+	}
+}
+
+long int filesize(FILE* fp) // функция определяет размер файла
+{
+	long int save_pos, size_of_file;
+	save_pos = ftell(fp);
+	fseek(fp, 0L, SEEK_END);
+	size_of_file = ftell(fp);
+	fseek(fp, save_pos, SEEK_SET);
+	return(size_of_file);
+}
